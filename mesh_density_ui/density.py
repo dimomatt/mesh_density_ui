591b569c6ada2786921ff8593fd05e62f661630e
Matthew <dimomatt@gmail.com>
2023-10-09 13:35:36 -0600

    Update mesh density function to use hyperbolic tangent
    This commit updates the density function from the 2D gaussian to the
    hyperbolic tangent function described by Ringler et al.

--- mesh_density_ui/density.py
+++ mesh_density_ui/density.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-# Example density function, returns the value of a 2D gaussian at lat and lon
+# Example density function, returns the value of the function described in
+#  Ringler, T. D., D. Jacobsen, M. Gunzburger, L. Ju, M. Duda, and W. Skamarock, 2011. “Exploring a
+# Multiresolution Modeling Approach within the Shallow-Water Equations”. Mon. Wea. Rev., 139, 3348
+# – 3368.
 def get_density(x, y):
-    sx = 10
-    sy = 10
-    mx = 0
-    my = 0
-    out = 1. / (2. * np.pi * sx * sy) * np.exp(-((y - mx)**2. / (2. * sx**2.) + (x - my)**2. / (2. * sy**2.)))
-    return out
+    minimum_density = 0
+    half_width_high_resolution = 10
+    transition_zone_width = 10
+    coefficient = (1 - minimum_density) / 2
+    to_calculate = (half_width_high_resolution - np.sqrt((x * x) + (y * y))) / transition_zone_width
+    return coefficient * (np.tanh(to_calculate) + 1) + minimum_density
